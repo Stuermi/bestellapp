e807d141e8f3cf7f8ba64f0961b4d39411e40d64
Constantin Stürmer <constantin.stuermer@googlemail.com>
2024-09-30 02:19:07 +0200

    added side templates

--- script/script.js
+++ script/script.js
@@ -1,49 +1,51 @@
-function init(){
+let basket = [];
+
+function init() {
     // getFromLocalStorage();
     renderMainContent();
     renderMenuCategory();
     renderMenu();
 }
 
-function renderMainContent(){
+function renderMainContent() {
     let contentRef = document.getElementById('main_content');
     contentRef.innerHTML = '';
     contentRef.innerHTML = getContentTemplate();
 }
 
-function renderMenuCategory(){
+function renderMenuCategory() {
     let menuCategoryContentRef = document.getElementById('menu_categories');
-        menuCategoryContentRef.innerHTML = '';
+    menuCategoryContentRef.innerHTML = '';
 
-        for (let menuCategoryIndex = 0; menuCategoryIndex < pizzeria.menuCategory.length; menuCategoryIndex++) {
-            menuCategoryContentRef.innerHTML += getMenuCategoriesTemplate(menuCategoryIndex);
-            loadMenuCategory(menuCategoryIndex)
-        }
+    for (let menuCategoryIndex = 0; menuCategoryIndex < pizzeria.menuCategory.length; menuCategoryIndex++) {
+        menuCategoryContentRef.innerHTML += getMenuCategoriesTemplate(menuCategoryIndex);
+        loadMenuCategory(menuCategoryIndex)
+    }
 }
 
-function loadMenuCategory(menuCategoryIndex){
+function loadMenuCategory(menuCategoryIndex) {
     let menuCategoryRef = document.getElementById(`menu_category${menuCategoryIndex}`);
     menuCategoryRef.innerHTML += pizzeria.menuCategory[menuCategoryIndex].category;
 }
 
-function renderMenu(){
+function renderMenu() {
     let menuContentRef = document.getElementById('menu');
     menuContentRef.innerHTML = '';
 
     for (let menuCategoryIndex = 0; menuCategoryIndex < pizzeria.menuCategory.length; menuCategoryIndex++) {
-            menuContentRef.innerHTML += getMenuTemplate(menuCategoryIndex)
-            loadMenuName(menuCategoryIndex);
-            renderDish(menuCategoryIndex);
-        }
+        menuContentRef.innerHTML += getMenuTemplate(menuCategoryIndex)
+        loadMenuName(menuCategoryIndex);
+        renderDish(menuCategoryIndex);
+    }
 }
 
-function loadMenuName(menuCategoryIndex){
+function loadMenuName(menuCategoryIndex) {
     let menuCategoryRef = document.getElementById(`menu_name${menuCategoryIndex}`);
     menuCategoryRef.innerHTML = '';
     menuCategoryRef.innerHTML += pizzeria.menuCategory[menuCategoryIndex].category;
 }
 
-function renderDish(menuCategoryIndex){
+function renderDish(menuCategoryIndex) {
     let dishContentRef = document.getElementById(`dish_content${menuCategoryIndex}`);
     for (let dishIndex = 0; dishIndex < pizzeria.menuCategory[menuCategoryIndex].items.length; dishIndex++) {
         dishContentRef.innerHTML += getDishTemplate(menuCategoryIndex, dishIndex);
@@ -51,11 +53,102 @@ function renderDish(menuCategoryIndex){
     }
 }
 
-function loadDish(menuCategoryIndex, dishIndex){
+function loadDish(menuCategoryIndex, dishIndex) {
     let pizzaNameRef = document.getElementById(`dish_name${menuCategoryIndex}${dishIndex}`);
     let pizzaIngredientsRef = document.getElementById(`ingredients${menuCategoryIndex}${dishIndex}`);
     let pizzaPriceRef = document.getElementById(`dish_price${menuCategoryIndex}${dishIndex}`);
     pizzaNameRef.innerHTML = pizzeria.menuCategory[menuCategoryIndex].items[dishIndex].dish;
     pizzaIngredientsRef.innerHTML = pizzeria.menuCategory[menuCategoryIndex].items[dishIndex].ingredients;
-    pizzaPriceRef.innerHTML = pizzeria.menuCategory[menuCategoryIndex].items[dishIndex].priceDish.toFixed(2).replace('.',',')+ ' €';    
+    pizzaPriceRef.innerHTML = pizzeria.menuCategory[menuCategoryIndex].items[dishIndex].priceDish.toFixed(2).replace('.', ',') + ' €';
+}
+
+function addDishToBasket(menuCategoryIndex, dishIndex) {
+    if (!checkDishInBasket(menuCategoryIndex, dishIndex)) {
+        basket.push({
+            "dish": pizzeria.menuCategory[menuCategoryIndex].items[dishIndex].dish,
+            "quantity": 1,
+            "dish_price": pizzeria.menuCategory[menuCategoryIndex].items[dishIndex].priceDish,
+        })
+    } else if (checkDishInBasket(menuCategoryIndex, dishIndex)) {
+        for (let basketIndex = 0; basketIndex < basket.length; basketIndex++) {
+            if (basket[basketIndex].dish === pizzeria.menuCategory[menuCategoryIndex].items[dishIndex].dish){
+                basket[basketIndex].quantity++;
+            }
+        }
+    }
+    renderFilledBasket();
+}
+
+function checkDishInBasket(menuCategoryIndex, dishIndex) {
+    let dishIsInBasket = false;
+    for (let basketIndex = 0; basketIndex < basket.length; basketIndex++) {
+        if (basket[basketIndex].dish == pizzeria.menuCategory[menuCategoryIndex].items[dishIndex].dish) {
+            dishIsInBasket = true
+        }
+    }
+    return dishIsInBasket;
+}
+
+// Funktion um den Inhalt des filledBasket zu laden
+function renderFilledBasket() {
+    let filledBasketContentRef = document.getElementById('filled_basket_content');
+    filledBasketContentRef.innerHTML ='';
+    for (let basketIndex = 0; basketIndex < basket.length; basketIndex++) {
+        filledBasketContentRef.innerHTML += getFilledBasketTemplate(basketIndex);
+        loadFilledBasket(basketIndex);
+    }
+    displayCorrectBasket();
+}
+
+function loadFilledBasket(basketIndex){
+    let filledBasketDishRef = document.getElementById(`filled_basket_dish${basketIndex}`);
+    let filledBasketQuantityRef = document.getElementById(`filled_basket_quantity${basketIndex}`);
+    let filledBasketDishPriceRef = document.getElementById(`filled_basket_dish_price${basketIndex}`);
+    filledBasketDishRef.innerHTML = basket[basketIndex].dish;
+    filledBasketQuantityRef.innerHTML = basket[basketIndex].quantity;
+    filledBasketDishPriceRef.innerHTML = basket[basketIndex].dish_price.toFixed(2).replace('.',',') + ' €';
+}
+
+function displayCorrectBasket() {
+    if (basket.length >= 1) {
+        addDNone('empty_basket');
+        removeDNone('filled_basket');
+    } else if (basket.length == 0) {
+        addDNone('filled_basket');
+        removeDNone('empty_basket');
+    }
+}
+
+function addDNone(id) {
+    let elementToBeRemoved = document.getElementById(id);
+    elementToBeRemoved.classList.add('d_none');
+}
+
+function removeDNone(id) {
+    let elementToBeRemoved = document.getElementById(id);
+    elementToBeRemoved.classList.remove('d_none');
+}
+
+// // Funktion um den Preis zu berechnen
+// function calculatePrice() {
+
+// }
+
+// // Funktion um Gericht aus dem Warenkorb zu entfernen
+// function removeDishFromBasket() {
+
+// }
+
+// // Funktion um die Menge um 1 zu erhöhen
+// function addAmountToBasket() {
+
+// }
+
+// // Funktion um die Menge um 1 zu verringern
+// function removeAmountFromBasket() {
+
+// }
+
+function bubblingProtection(event) {
+    event.stopPropagation();
 }
--- script/template.js
+++ script/template.js
@@ -1,4 +1,4 @@
-function getContentTemplate(menuCategoryIndex){
+function getContentTemplate(menuCategoryIndex) {
     return `<section class="main_section">
             <div class="header_image_container">
                 <img class="header_image" src="./assets/img/pizza.jpg" alt="Pizza Bild">
@@ -11,9 +11,9 @@ function getContentTemplate(menuCategoryIndex){
                     <p>Bewertung (4,2 von 5 <img class="rating_star" src="./assets/icon/star_color.png"
                             alt="Stern Orange">)</p>
                     <img title="Mindestbestellwert" class="bag_icon" src="assets/icon/bag.png" alt="Shopping Bag Icon">
-                    <p>${pizzeria.minimumOrder.toFixed(2).replace('.',',')} €</p>
+                    <p>${pizzeria.minimumOrder.toFixed(2).replace('.', ',')} €</p>
                     <img title="Lieferkosten" class="delivery_icon" src="assets/icon/bike.png" alt="Bike Icon">
-                    <p>${pizzeria.deliveryCost.toFixed(2).replace('.',',')} €</p>
+                    <p>${pizzeria.deliveryCost.toFixed(2).replace('.', ',')} €</p>
                 </div>
             </div>
 
@@ -28,29 +28,14 @@ function getContentTemplate(menuCategoryIndex){
             </div>
             <hr>
             <div class="basket">
-                <div class="empty_basket">
+                <div id="empty_basket" class="empty_basket">
                     <img class="basket_icon" src="./assets/icon/shopping-basket.png" alt="Basket Icon">
                     <h2>Fülle deinen Warenkorb</h2>
                     <p>Füge leckere Gerichte aus der Speisekarte hinzu und bestelle dein Essen</p>
                 </div>
-                <div class="filled_basket d_none">
-                    <div class="filled_basket_header">
-                        <h2>name_dish</h2>
-                    </div>
-                    <div class="filled_basket_row">
-                        <div class="calculator">
-                            <div title="Menge reduzieren" class="calc_button">
-                                <img class="minus_icon_calc" src="./assets/icon/minus_icon_color.png" alt="Minus Icon">
-                            </div>
-                            <p>amount_x</p>
-                            <div title="Menge hinzufügen" class="calc_button">
-                                <img class="plus_icon_calc" src="./assets/icon/plus_icon_color.png" alt="Plus Icon">
-                            </div>
-                        </div>
-                        <p>price_dish</p>
-                        <img title="Gericht entfernen" class="trash_icon" src="./assets/icon/trash_can_color.png"
-                            alt="">
-                    </div>
+
+                <div id="filled_basket" class="filled_basket d_none">
+                <div id="filled_basket_content"></div>
                     <hr>
                     <div class="order_price">
                         <div class="subtotal">
@@ -71,29 +56,50 @@ function getContentTemplate(menuCategoryIndex){
         </section>`
 }
 
-function getMenuCategoriesTemplate(menuCategoryIndex){
+function getMenuCategoriesTemplate(menuCategoryIndex) {
     return `
                 <nav class="menu_category_selection">
                     <a id="menu_category${menuCategoryIndex}" href="#"></a>
                 </nav>`
 }
 
-function getMenuTemplate(menuCategoryIndex){
+function getMenuTemplate(menuCategoryIndex) {
     return `
-            <h2 id="menu_name${menuCategoryIndex}"></h2>
+            <h2 id="menu_name${menuCategoryIndex}" class="menu_header"></h2>
                 <div id="dish_content${menuCategoryIndex}" class="dish_content"></div>
 `
 }
 
-function getDishTemplate(menuCategoryIndex, dishIndex){
+function getDishTemplate(menuCategoryIndex, dishIndex) {
     return `
-                <div id="dish${menuCategoryIndex}${dishIndex}" class="dish">
-                    <h2 id="dish_name${menuCategoryIndex}${dishIndex}">gfege</h2>
-                    <p id="ingredients${menuCategoryIndex}${dishIndex}">geegeg</p>
-                    <span id="dish_price${menuCategoryIndex}${dishIndex}">ggegeeg</span>
-                    <div class="add_button">
+                <div onclick="addDishToBasket(${menuCategoryIndex}, ${dishIndex})" id="dish${menuCategoryIndex}${dishIndex}" class="dish">
+                    <h2 id="dish_name${menuCategoryIndex}${dishIndex}"></h2>
+                    <p id="ingredients${menuCategoryIndex}${dishIndex}"></p>
+                    <span id="dish_price${menuCategoryIndex}${dishIndex}"></span>
+                    <div onclick="bubblingProtection(event); addDishToBasket(${menuCategoryIndex}, ${dishIndex})" class="add_button">
                         <img class="plus_icon" src="./assets/icon/plus_icon_color.png" alt="Plus Icon">
                     </div>
                 </div>`
+}
+
+function getFilledBasketTemplate(basketIndex) {
+    return `
+                    <div class="filled_basket_header">
+                        <h2 id="filled_basket_dish${basketIndex}">name_dish</h2>
+                    </div>
+                    <div class="filled_basket_row">
+                        <div class="calculator">
+                            <div title="Menge reduzieren" class="calc_button">
+                                <img class="minus_icon_calc" src="./assets/icon/minus_icon_color.png" alt="Minus Icon">
+                            </div>
+                            <p id="filled_basket_quantity${basketIndex}">amount_x</p>
+                            <div title="Menge hinzufügen" class="calc_button">
+                                <img class="plus_icon_calc" src="./assets/icon/plus_icon_color.png" alt="Plus Icon">
+                            </div>
+                        </div>
+                        <p id="filled_basket_dish_price${basketIndex}">price_dish</p>
+                        <img title="Gericht entfernen" class="trash_icon" src="./assets/icon/trash_can_color.png"
+                            alt="">
+                    </div>`
 }
 
